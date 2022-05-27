61aa80687db260292da58f5507d1d3c2c6597cd5
Tommy67a <tommy.67a@gmail.com>
2022-05-27 15:24:06 +0200

    Movement and rotation added to Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody rig;
+   
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+
+        Vector3 vel = rig.velocity;
+        vel.y = 0;
+
+        if (vel.x !=0 || vel.z != 0)
+        {
+            transform.forward = vel;
+        }       
+    }
+}
