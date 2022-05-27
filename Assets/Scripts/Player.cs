b7a8478d1bc15ebfa3016ff6cc055d9b98f32f4d
Tommy67a <tommy.67a@gmail.com>
2022-05-27 15:50:55 +0200

    bool variable  isGrounded added to Player so no more double-jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,9 +25,18 @@ public class Player : MonoBehaviour
             transform.forward = vel;
         }  
         
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
+            isGrounded = false;
             rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contacts[0].normal == Vector3.up)
+        {
+            isGrounded = true;
         }
     }
 }
